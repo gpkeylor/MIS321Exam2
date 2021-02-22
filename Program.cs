1376e0ced643874409d422136f2c14abda3c92d7
gpkeylor <gpkeylor@crimson.ua.edu>
2021-02-21 20:59:49 -0600

    Test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Exam2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
